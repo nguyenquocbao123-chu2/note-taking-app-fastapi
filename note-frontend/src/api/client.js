fd0a4cbe7c6f03dfcb107ca8f30784e75e9bc9f7
nguyenquocbao123-chu2 <23050141@student.bdu.edu.vn>
2025-12-21 04:05:34 +0700

    fix dburl

--- note-frontend/src/api/client.js
+++ note-frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE =
-  import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
+  import.meta.env.VITE_API_BASE_URL || "https://note-taking-app-fastapi.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE,
